0b6bd3c4d9d32814acfa68068d7a7e7bbcb4c5eb
n-fukidome <n-fukidome@nekojarashi.com>
2015-10-10 08:05:55 +0900

    Storing tasks in a collection

--- simple-todos.js
+++ simple-todos.js
@@ -1,14 +1,27 @@
+Tasks = new Mongo.Collection("tasks");
+ 
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
 
+// # 1
+// if (Meteor.isClient) {
+//   // This code only runs on the client
+//   Template.body.helpers({
+//     tasks: [
+//       { text: "This is task 1" },
+//       { text: "This is task 2" },
+//       { text: "This is task 3" }
+//     ]
+//   });
+// }
+
+// # 0
 // if (Meteor.isClient) {
 //   // counter starts at 0
 //   Session.setDefault('counter', 0);
